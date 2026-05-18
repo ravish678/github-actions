496b54ac8f5a3fdf2a7ed4a0b5192e5d5c74e1ce
unknown <ravishkumar678@gmail.com>
2026-05-18 13:27:29 +0530

    Start Server

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
+// app.get('/', (req, res) => {
+//     res.send('CI/CD Demo: Deployed successfully via GitHub Actions & PM2!');
+// });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
